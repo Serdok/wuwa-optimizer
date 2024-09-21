5fbcc3eb24a23e9a55bae4e1831a20ed22c536d7
serdok <l.anass.pro@gmail.com>
2024-09-21 14:05:00 +0200

    do not display the stat is it is at 0 (or 0%)

--- src/routes/build/FinalStat.svelte
+++ src/routes/build/FinalStat.svelte
@@ -5,14 +5,16 @@
 	const { attribute, value, is_percentage = true }: { attribute: Attribute, value: number, is_percentage?: boolean } = $props();
 </script>
 
-<div class="flex flex-row justify-between gap-4">
-	<div class="flex flex-row items-center gap-1">
-		<img src="{STATS_ICONS[attribute]}" alt="{attribute}" class="w-6"/>
-		<div class="text-lg">{attribute}</div>
+{#if value !== 0}
+	<div class="flex flex-row justify-between gap-4">
+		<div class="flex flex-row items-center gap-1">
+			<img src="{STATS_ICONS[attribute]}" alt="{attribute}" class="w-6"/>
+			<div class="text-lg">{attribute}</div>
+		</div>
+		{#if is_percentage}
+			<div class="text-lg">{value.toFixed(1)}%</div>
+		{:else}
+			<div class="text-lg">{value.toFixed(0)}</div>
+		{/if}
 	</div>
-	{#if is_percentage}
-		<div class="text-lg">{value.toFixed(1)}%</div>
-	{:else}
-		<div class="text-lg">{value.toFixed(0)}</div>
-	{/if}
-</div>
+{/if}

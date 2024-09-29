b1b2f991264992ddf014affa784ff0e6e816f022
serdok <l.anass.pro@gmail.com>
2024-09-29 23:45:59 +0200

    moved mock echoes to its own file for reuse

--- src/lib/optimizer/generate_build.test.ts
+++ src/lib/optimizer/generate_build.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from 'vitest';
 import { type Echo, EchoClass, Sonata } from '$lib/types/echo';
 import { generate_combinations, is_valid_combination } from '$lib/optimizer/generate_build';
 import { Attribute } from '$lib/types/stat';
+import MOCK_ECHOES from './MOCK_ECHOES.json' with { type: 'json' };
+const MOCK = MOCK_ECHOES as Echo[];
 
 describe('generate combinations', () => {
 	describe('general use cases', () => {
@@ -68,114 +70,6 @@ describe('generate combinations', () => {
 });
 
 describe('validate combination constraints', () => {
-	const MOCK: Echo[] = [
-		{
-			id: '4a428f61-a88d-4b7e-bdcc-3af660df05ab',
-			name: 'Fusion Dreadmane',
-			quality: 5,
-			sonata: Sonata.MoltenRift,
-			class: EchoClass.Common,
-			cost: 1,
-			level: 25,
-			main_stat: {
-				primary: { attribute: Attribute.ATK_P, value: 18 },
-				secondary: { attribute: Attribute.HP, value: 2280 }
-			},
-			sub_stats: [
-				{ attribute: Attribute.DEF, value: 60 },
-				{ attribute: Attribute.CritDamage, value: 15 },
-				{ attribute: Attribute.ATK, value: 30 },
-				{ attribute: Attribute.CritRate, value: 8.7 },
-				{ attribute: Attribute.EnergyRegen, value: 10 }
-			],
-			equipped_by: null
-		},
-		{
-			id: 'be20755b-1ad0-4024-9a03-611aba7e6900',
-			name: 'Fusion Warrior',
-			quality: 5,
-			sonata: Sonata.MoltenRift,
-			class: EchoClass.Common,
-			cost: 1,
-			level: 25,
-			main_stat: {
-				primary: { attribute: Attribute.ATK_P, value: 18 },
-				secondary: { attribute: Attribute.HP, value: 2280 }
-			},
-			sub_stats: [
-				{ attribute: Attribute.CritDamage, value: 15 },
-				{ attribute: Attribute.HP, value: 430 },
-				{ attribute: Attribute.CritRate, value: 7.5 },
-				{ attribute: Attribute.ResonanceLiberationBonus, value: 8.6 },
-				{ attribute: Attribute.DEF_P, value: 10.9 }
-			],
-			equipped_by: null
-		},
-		{
-			id: 'e636a200-7aa6-4951-905b-a3539c85746d',
-			name: 'Havoc Dreadmane',
-			quality: 5,
-			sonata: Sonata.MoltenRift,
-			class: EchoClass.Elite,
-			cost: 3,
-			level: 25,
-			main_stat: {
-				primary: { attribute: Attribute.FusionBonus, value: 30 },
-				secondary: { attribute: Attribute.ATK, value: 100 }
-			},
-			sub_stats: [
-				{ attribute: Attribute.CritDamage, value: 21 },
-				{ attribute: Attribute.ResonanceLiberationBonus, value: 10.9 },
-				{ attribute: Attribute.ATK, value: 40 },
-				{ attribute: Attribute.HP_P, value: 8.6 },
-				{ attribute: Attribute.CritRate, value: 6.9 }
-			],
-			equipped_by: null
-		},
-		{
-			id: '9d4e0c02-4962-4907-b252-ea195da27c29',
-			name: 'Violet-Feathered Heron',
-			quality: 5,
-			sonata: Sonata.MoltenRift,
-			class: EchoClass.Elite,
-			cost: 3,
-			level: 25,
-			main_stat: {
-				primary: { attribute: Attribute.ATK_P, value: 30 },
-				secondary: { attribute: Attribute.ATK, value: 100 }
-			},
-			sub_stats: [
-				{ attribute: Attribute.CritRate, value: 7.5 },
-				{ attribute: Attribute.ATK_P, value: 7.1 },
-				{ attribute: Attribute.CritDamage, value: 17.4 },
-				{ attribute: Attribute.HP_P, value: 9.4 },
-				{ attribute: Attribute.HeavyAttackBonus, value: 7.9 }
-			],
-			equipped_by: null
-		},
-		{
-			id: '89ea59ce-9182-4ceb-b640-04472199192c',
-			name: 'Inferno Rider',
-			quality: 5,
-			sonata: Sonata.MoltenRift,
-			class: EchoClass.Overlord,
-			cost: 4,
-			level: 25,
-			main_stat: {
-				primary: { attribute: Attribute.CritDamage, value: 44 },
-				secondary: { attribute: Attribute.ATK, value: 150 }
-			},
-			sub_stats: [
-				{ attribute: Attribute.CritDamage, value: 15 },
-				{ attribute: Attribute.HP_P, value: 10.1 },
-				{ attribute: Attribute.DEF, value: 60 },
-				{ attribute: Attribute.CritRate, value: 6.9 },
-				{ attribute: Attribute.ResonanceLiberationBonus, value: 9.4 }
-			],
-			equipped_by: null
-		}
-	];
-
 	describe('general use cases', () => {
 		it('should return false if the total cost exceeds 12', () => {
 			const build: Echo[] = [

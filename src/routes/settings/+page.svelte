2858c8569c624236480f8d37c8e33c0b1b38d7da
serdok <l.anass.pro@gmail.com>
2025-03-29 12:53:33 +0100

    feat: import echoes from JSON

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,7 +2,11 @@
 	import { db } from '$lib/db';
 
 	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import { toast } from 'svelte-sonner';
+	import type { Echo } from '$lib/data/echoes/types';
+
+	let import_file: FileList | undefined = $state();
 
 	async function export_echoes() {
 		const echoes = await db.echoes.toArray();
@@ -15,11 +19,35 @@
 		link.click();
 	}
 
+	async function import_echoes() {
+		if (!import_file) {
+			toast.error('no file was provided');
+			return;
+		}
+
+		try {
+			const file = import_file[0];
+			const contents: Echo[] = JSON.parse(await file.text());
+			await db.echoes.bulkAdd(contents);
+			toast.message('done');
+		} catch (e) {
+			const err = e as Error;
+			console.error(e);
+			toast.error(err.message);
+		}
+	}
+
 	async function delete_echoes() {
 		await db.echoes.clear();
 		toast.message('done');
 	}
 </script>
 
-<Button type="button" variant="default" onclick={() => export_echoes()}>export echoes</Button>
-<Button type="button" variant="destructive" onclick={() => delete_echoes()}>delete all echoes</Button>
+<div class="flex flex-col space-y-4 max-w-[25%]">
+	<Button type="button" variant="default" onclick={() => export_echoes()}>export echoes</Button>
+	<div class="flex flex-row space-x-2">
+		<Input type="file" accept="application/json" bind:files={import_file} />
+		<Button type="button" variant="default" onclick={() => import_echoes()} disabled={import_file === undefined}>import echoes</Button>
+	</div>
+	<Button type="button" variant="destructive" onclick={() => delete_echoes()}>delete all echoes</Button>
+</div>

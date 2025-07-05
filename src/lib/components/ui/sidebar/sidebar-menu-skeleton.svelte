6e895fa29240db3184a6bace8d57c39df5da17fb
serdok <l.anass.pro@gmail.com>
2025-07-05 20:47:59 +0200

    feat: migrating to tailwindcss v4

--- src/lib/components/ui/sidebar/sidebar-menu-skeleton.svelte
+++ src/lib/components/ui/sidebar/sidebar-menu-skeleton.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Skeleton } from "$lib/components/ui/skeleton/index.js";
-	import { cn } from "$lib/utils.js";
-	import type { WithElementRef } from "bits-ui";
+	import { cn, type WithElementRef } from '$lib/utils.js';
 	import type { HTMLAttributes } from "svelte/elements";
 
 	let {
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		tailwindcss(),
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',

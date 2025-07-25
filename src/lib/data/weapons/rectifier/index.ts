cc5444d1c4445f93c5240a7d60c3b73332ae1aff
serdok <l.anass.pro@gmail.com>
2025-07-25 03:41:05 +0200

    feat: cantarella + whispers of sirens

--- src/lib/data/weapons/rectifier/index.ts
+++ src/lib/data/weapons/rectifier/index.ts
@@ -1,5 +1,7 @@
 import luminous_hymn from './luminous_hymn';
+import whispers_of_sirens from './whispers_of_sirens';
 
 export const RECTIFIERS = {
 	luminous_hymn,
+	whispers_of_sirens,
 } as const;

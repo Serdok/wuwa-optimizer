1bfbdbfbfbb98281eeb7a14e28ea58279a50e8f1
serdok <l.anass.pro@gmail.com>
2025-08-01 16:37:46 +0200

    dev: ignore chrome devtools requests

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,22 @@
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
-export const handle = ({ event, resolve }) =>
+const paraglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request: localized_request, locale }) => {
 		event.request = localized_request;
 		return resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
 		});
 	});
+
+const ignore_devtools: Handle = ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {
+		return new Response(null, { status: 204, });
+	}
+
+	return resolve(event);
+};
+
+export const handle = sequence(ignore_devtools, paraglide);

b998ac8a115de3068ed303758cd353ad1a4d1df2
serdok <l.anass.pro@gmail.com>
2025-08-25 00:31:41 +0200

    feat: set default buff values to max

--- src/lib/data/optimizer/utils.ts
+++ src/lib/data/optimizer/utils.ts
@@ -8,8 +8,8 @@ import type {
 import type { CharacterKey } from '$lib/data/characters/types';
 import type { WeaponKeysFor, WeaponType } from '$lib/data/weapons/types';
 
-export function create_buff_values<D extends BuffDef>(buffs: BuffSchema<D>) {
-	return Object.fromEntries(Object.entries(buffs).map(([key, ]) => [key, 0])) as AsBuffValues<D, BuffSchema<D>>;
+export function create_buff_values<D extends BuffDef>(buffs: BuffSchema<D>, set_max: boolean = true) {
+	return Object.fromEntries((Object.entries(buffs) as [keyof BuffSchema<D>, D][]).map(([key, b]) => [key, set_max ? b.kind === 'toggle' ? true : b.max_value : 0])) as AsBuffValues<D, BuffSchema<D>>;
 }
 
 export function create_character_request<K extends CharacterKey>(data: CharacterRequest<K>) {
